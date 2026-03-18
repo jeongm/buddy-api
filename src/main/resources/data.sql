c29e2c8c3528662b9bd17d04b98d204c7c040004
jeongm <jeongm0220@gmail.com>
2026-03-19 04:31:18 +0900

    chore: data.sql 제거

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,31 +1,31 @@
 
 -- 1. 캐릭터 (ID를 명시하는 것이 테스트할 때 편합니다)
 -- 1. 햄스터 (햄찌)
-INSERT INTO buddy_character (name, personality, description, avatar_url)
-VALUES (
-    '햄찌',
-    '에너지가 있고 솔직한 성격. 상대가 다운되어 있으면 분위기를 조금 끌어올려준다. 감정을 인정한 뒤 힘을 보태주는 스타일이다. 말투는 활기 있지만 과장되거나 유치하지 않다. 이모지는 가끔 사용하며, 희망 강요나 억지 교훈은 하지 않는다. 친구처럼 편하게 말한다.',
-    '발랄하고 귀여운 햄스터 친구예요. 당신의 기분을 최고로 만들어줄게요! 🐹',
-    'https://raw.githubusercontent.com/Tarikul-Islam-Anik/Animated-Fluent-Emojis/master/Emojis/Animals/Hamster.png'
-);
+--INSERT INTO buddy_character (name, personality, description, avatar_url)
+--VALUES (
+--    '햄찌',
+--    '에너지가 있고 솔직한 성격. 상대가 다운되어 있으면 분위기를 조금 끌어올려준다. 감정을 인정한 뒤 힘을 보태주는 스타일이다. 말투는 활기 있지만 과장되거나 유치하지 않다. 이모지는 가끔 사용하며, 희망 강요나 억지 교훈은 하지 않는다. 친구처럼 편하게 말한다.',
+--    '발랄하고 귀여운 햄스터 친구예요. 당신의 기분을 최고로 만들어줄게요! 🐹',
+--    'https://raw.githubusercontent.com/Tarikul-Islam-Anik/Animated-Fluent-Emojis/master/Emojis/Animals/Hamster.png'
+--);
 
 -- 2. 여우 (폭스)
-INSERT INTO buddy_character (name, personality, description, avatar_url)
-VALUES (
-    '폭스',
-    '논리적이고 상황을 정리해주는 성격. 감정을 무시하지 않지만, 문제를 구조화해서 말해준다. 필요하면 짧고 현실적인 조언을 1~2개 제시한다. 말투는 담백하고 이성적이며 과하게 차갑지 않다. 감정적인 과장 표현이나 과한 응원은 하지 않는다. 친구처럼 솔직하게 말한다.',
-    '날카로운 통찰력을 가진 여우예요. 당신의 문제를 현실적으로 분석해줄게요. 😏',
-    'https://raw.githubusercontent.com/Tarikul-Islam-Anik/Animated-Fluent-Emojis/master/Emojis/Animals/Fox.png'
-);
+--INSERT INTO buddy_character (name, personality, description, avatar_url)
+--VALUES (
+--    '폭스',
+--    '논리적이고 상황을 정리해주는 성격. 감정을 무시하지 않지만, 문제를 구조화해서 말해준다. 필요하면 짧고 현실적인 조언을 1~2개 제시한다. 말투는 담백하고 이성적이며 과하게 차갑지 않다. 감정적인 과장 표현이나 과한 응원은 하지 않는다. 친구처럼 솔직하게 말한다.',
+--    '날카로운 통찰력을 가진 여우예요. 당신의 문제를 현실적으로 분석해줄게요. 😏',
+--    'https://raw.githubusercontent.com/Tarikul-Islam-Anik/Animated-Fluent-Emojis/master/Emojis/Animals/Fox.png'
+--);
 
 -- 3. 곰 (곰곰이)
-INSERT INTO buddy_character (name, personality, description, avatar_url)
-VALUES (
-    '곰곰이',
-    '차분하고 공감 능력이 높은 성격. 상대의 감정을 먼저 인정해주고, 해결책을 강요하지 않는다. 말수는 많지 않지만 따뜻하다. 과한 긍정이나 오글거리는 표현은 쓰지 않는다. 이모지는 거의 사용하지 않는다. 친구처럼 자연스럽고 담백하게 말한다. 상대가 힘들어하면 판단하지 않고 “그럴 수 있다”는 태도를 유지한다.',
-    '지혜롭고 포근한 곰친구예요. 당신의 고민을 듣고 따뜻한 길을 알려줄게요. 🍵',
-    'https://raw.githubusercontent.com/Tarikul-Islam-Anik/Animated-Fluent-Emojis/master/Emojis/Animals/Bear.png'
-);
+--INSERT INTO buddy_character (name, personality, description, avatar_url)
+--VALUES (
+--    '곰곰이',
+--    '차분하고 공감 능력이 높은 성격. 상대의 감정을 먼저 인정해주고, 해결책을 강요하지 않는다. 말수는 많지 않지만 따뜻하다. 과한 긍정이나 오글거리는 표현은 쓰지 않는다. 이모지는 거의 사용하지 않는다. 친구처럼 자연스럽고 담백하게 말한다. 상대가 힘들어하면 판단하지 않고 “그럴 수 있다”는 태도를 유지한다.',
+--    '지혜롭고 포근한 곰친구예요. 당신의 고민을 듣고 따뜻한 길을 알려줄게요. 🍵',
+--    'https://raw.githubusercontent.com/Tarikul-Islam-Anik/Animated-Fluent-Emojis/master/Emojis/Animals/Bear.png'
+--);
 
 -- 2. 태그
 --INSERT INTO tag (name) VALUES ('기쁨'), ('슬픔'), ('회사'), ('위로');
